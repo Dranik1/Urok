1ff68b8c01257d8972c8133139717866d6a1578a
Dranik1 <125028857+Dranik1@users.noreply.github.com>
2024-09-16 11:04:31 +0300

    Update mantosana.py

--- mantosana.py
+++ mantosana.py
@@ -10,17 +10,28 @@ class Person():
 
 
 class Student(Person):
+    def __init__(self, name, age):
+        Person.__init__(self, name, age)
+        print("Student created")
     def study(self):
         print(f'{self.name} studies')
+    def say_hello(self):
+        print(f'Student with name: {self.name} says hello!')
+
+class Grade(Student):
+    def olympic(self):
+        print(f"{self.name} participates in olympic")
+
 
 class Teacher(Person):
     def teach(self):
         print(f'{self.name} teaches')
 
 
-p1=Student("Tom", 15)
+p1=Grade("Tom", 15)
 t1=Teacher("Katy", 25)
 p1.say_hello()
 t1.say_hello()
 p1.study()
 t1.teach()
+p1.olympic()
