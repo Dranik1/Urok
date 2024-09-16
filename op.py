85d1fefc52ca43f352ecc4333ef6d9fbb69d8fab
Dranik1 <125028857+Dranik1@users.noreply.github.com>
2024-09-16 09:18:08 +0300

    Update op.py

--- op.py
+++ op.py
@@ -6,6 +6,25 @@ class Person:
     def output(self):
         print(self.name, self.age)
 
-p=Person()
+'''p=Person()
 p.set("Peter", 20)
 p.output()
+
+d=Person()
+d.set("Jan", 37)
+d.output()
+
+a=input("Ievadiet vārdu: ")
+b=int(input("Ievadiet vecumu: "))
+c=Person()
+c.set(a, b)
+c.output()'''
+
+p=Person()
+sk=int(input("Ievadiet cilveku skaitu: "))
+for i in range(sk):
+    a=input("Ievadiet vārdu: ")
+    b=int(input("Ievadiet vecumu: "))
+    c=Person()
+    c.set(a, b)
+    c.output()
