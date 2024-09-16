7e0ff8358484654fbd5bdb50e7ce5e4d1a263484
Dranik1 <125028857+Dranik1@users.noreply.github.com>
2024-09-16 09:25:02 +0300

    Create podarok.py

---
+++ podarok.py
@@ -0,0 +1,11 @@
+class davana():
+    def info(self, nosaukums, cena):
+        self.nosaukums=nosaukums
+        self.cena=cena
+
+    def output(self):
+        print( self.nosaukums, self.cena)
+
+d=davana()
+d.info('Lego', 29.99)
+d.output()
