f5de15a55154d5340135f47f45709a55f5c2413b
Dranik1 <125028857+Dranik1@users.noreply.github.com>
2024-09-16 11:09:38 +0300

    Update mantosana.py

--- mantosana.py
+++ mantosana.py
@@ -10,8 +10,9 @@ class Person():
 
 
 class Student(Person):
-    def __init__(self, name, age):
-        Person.__init__(self, name, age)
+    def __init__(self, name, age, average_grade):
+        super().__init__(name, age)
+        self.average_grade=average_grade
         print("Student created")
     def study(self):
         print(f'{self.name} studies')
@@ -28,10 +29,22 @@ class Teacher(Person):
         print(f'{self.name} teaches')
 
 
-p1=Grade("Tom", 15)
+def introduce(person):
+    person.say_hello()
+
+people_arr=[Student("Tom", 15, 4.5), Teacher("Katy", 25), Grade("Bob", 26, 4.8)]
+
+
+for person in people_arr:
+    introduce(person)
+
+
+
+
+'''p1=Grade("Tom", 15)
 t1=Teacher("Katy", 25)
 p1.say_hello()
 t1.say_hello()
 p1.study()
 t1.teach()
-p1.olympic()
+p1.olympic()'''
