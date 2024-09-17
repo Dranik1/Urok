c6ae7f4f35a6d3416076f7f47a263c020a14e467
Dranik1 <125028857+Dranik1@users.noreply.github.com>
2024-09-17 16:42:19 +0300

    Create mantosana_auto.py

---
+++ mantosana_auto.py
@@ -0,0 +1,35 @@
+class Vehicle:
+    def __init__(self, make, model, year, price):
+        self.make=make
+        self.model=model
+        self.year=year
+        self.price=price
+
+    def total_vehicles(self):
+        Vehicle.total+=self.price
+        print(f"Auto cena ir {Vehicle.total}")
+        return Vehicle.total
+
+    def display_info(self):
+        print(f"Marka: {self.make}"
+              f"\nModele: {self.model}"
+              f"\nIzlaiduma gads: {self.year}"
+              f"\nCena: {self.price} euro")
+
+class Car(Vehicle):
+    def __ini__(self, make, model, year, price, num_doors, body_style):
+        super().__init__(make, model, year, price)
+        self.num_doors=num_doors
+        self.body_style=body_style
+
+class Truck(Vehicle):
+    def __ini__(self, make, model, year, price, bed_length, towing_capacity):
+        super().__init__(make, model, year, price)
+        self.bed_length=bed_length
+        self.towing_capacity=towing_capacity
+
+car=Car("Toyota", "Camry", 2022, 29000, 4, "sedan")
+car.display_info()
+
+truck=Truck("Ford", "F-Max", 2013, 30000, "6162", "13 t")
+truck.display_info()
